5d7503b073c556ede2367f4e559b19bcb16e7af2
Josimar Goncalves <foxchanger@hotmail.com>
2021-12-08 21:53:08 -0300

    Update Initial

---
+++ Flappy Bird (Fox)/Assets/Scripts/personagem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class personagem : MonoBehaviour
+{
+    public float forcaImpulso;
+    public static bool jogando;
+        Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0.0f;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            if (!jogando ) 
+            {
+                rb.gravityScale = 1.0f;
+                jogando = true;
+                
+            }
+            rb.velocity = new Vector2(0.0f, 0.0f);
+            rb.AddForce(Vector2.up * forcaImpulso);
+
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.y * 2.0f);
+
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        placar.pontos++;
+    }
+
+}
---
+++ Flappy Bird (Fox)/Assets/Scripts/placar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class placar : MonoBehaviour {
+    public Text txtPontos;
+    public static int pontos;
+	// Use this for initialization
+	void Start () {
+        pontos = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!personagem.jogando)
+        {
+            pontos = 0;
+        }
+        txtPontos.text = pontos.ToString();
+		
+	}
+
+    
+}
